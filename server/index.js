d731b2220ab86388a4be98fc469da12025664a45
Joseph <JoshyCristiano@outlook.es>
2025-07-03 23:59:33 -0500

    Test deploy 5

--- server/index.js
+++ server/index.js
@@ -3,5 +3,5 @@ import {getConnection} from './src/database/connection.js';
 
 getConnection();
 
-app.listen(procces.env.PORT || 3000);
-console.log('Server on port', procces.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
+console.log('Server on port', process.env.PORT || 3000);
